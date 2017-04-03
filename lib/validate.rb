7384cba778b95aa741dd2348e9bd4f3007242192
rongxanh88 <rongxanh88@gmail.com>
2017-04-03 13:06:30 -0600

    add validate coordinates in line

--- lib/validate.rb
+++ lib/validate.rb
@@ -11,7 +11,6 @@ class Validate
   def validate(coordinate)
     split_coordinate = coordinate.split("")
     rows = Hash[ROWS.sort_by { |key,value| value }[0...board.size]]
-    p rows
     row = split_coordinate[0]
     col = split_coordinate[1].to_i
     if col < 1 or col > board.size
@@ -21,5 +20,22 @@ class Validate
     else
       true
     end
+  end
+
+  def validate_in_line(first_coord, second_coord)
+    first_split = first_coord.split("")
+    second_split = second_coord.split("")
+    letter, number = 0, 1
+
+    if (first_split[letter] == second_split[letter]) or
+      (first_split[number] == second_split[number])
+      true
+    else
+      false
+    end
+  end
+
+  def validate_all_empty(first_coord, second_cood)
+
   end
 end
--- test/validate_test.rb
+++ test/validate_test.rb
@@ -10,7 +10,7 @@ class ValidateTest < Minitest::Test
     assert_instance_of Validate, validator
   end
   
-  def test_validate_coordinates
+  def test_validate_coordinate
     board = Board.new(4)
     validator = Validate.new(board)
     assert validator.validate("A1")
@@ -21,5 +21,22 @@ class ValidateTest < Minitest::Test
     refute validator.validate("B0")
     refute validator.validate("E9")
     refute validator.validate("F-10")
+  end
+
+  def test_validate_coordinates_are_in_line
+    board = Board.new(4)
+    validator = Validate.new(board)
+    assert validator.validate_in_line("A1", "A3")
+    assert validator.validate_in_line("A1", "B1")
+    assert validator.validate_in_line("D2", "D3")
+    refute validator.validate_in_line("A1", "B3")
+    refute validator.validate_in_line("C1", "D3")
+    refute validator.validate_in_line("E1", "D3")
+  end
+
+  def test_validate_all_empty
+    board = Board.new(4)
+    validator = Validate.new(board)
+    assert validator.validate_all_empty("A1", "A3")
   end
 end
